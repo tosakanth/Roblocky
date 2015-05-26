f94b62427099b0c2559e92675b8076a6af16c8d1
Tosakanth Project <tosakun.meeting@gmail.com>
2015-05-26 11:05:41 +0700

    Update views.py

--- webapplication/tkrobot/views.py
+++ webapplication/tkrobot/views.py
@@ -1,1 +1,28 @@
-#-- just beginning.
+from flask import render_template,request,send_from_directory,redirect
+import os
+from . import tkrobot
+
+#same object defined in __init__.py
+@tkrobot.route('/')
+def show_intro():
+	return render_template('index.html');
+	
+@tkrobot.route('/js/<path:filename>')
+def send_script(filename):
+   return send_from_directory(os.getcwd()+'/tkrobot/static/js',filename)	
+
+@tkrobot.route('/images/<path:filename>')
+def send_images(filename):
+   return send_from_directory(os.getcwd()+'/tkrobot/static/images',filename)	
+
+@tkrobot.route('/media/<path:filename>')
+def send_media(filename):
+   return send_from_directory(os.getcwd()+'/tkrobot/static/media',filename)	
+
+@tkrobot.route('/css/<path:filename>')
+def send_css(filename):
+   return send_from_directory(os.getcwd()+'/tkrobot/static/css',filename)	
+   
+@tkrobot.route('/img/<path:filename>')
+def send_img(filename):
+   return send_from_directory(os.getcwd()+'/tkrobot/static/img',filename)	
