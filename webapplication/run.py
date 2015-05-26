d70c77d4ac7b62cdaab46b3b0762bdc4270f7a35
Tosakanth Project <tosakun.meeting@gmail.com>
2015-05-26 10:55:10 +0700

    Update run.py

--- webapplication/run.py
+++ webapplication/run.py
@@ -1,1 +1,11 @@
-#---
+#!/usr/bin/python
+
+#from app import app
+from flask import Flask
+from tkrobot import tkrobot
+
+app=Flask(__name__)
+app.register_blueprint(tkrobot)
+
+app.debug=True
+app.run(host='0.0.0.0',port=int(8080))
